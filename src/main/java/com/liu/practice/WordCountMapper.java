b989859d2dc13f4367f3af5a8f4b25aeb0cc5b50
刘俊辉 <1157757077@qq.com>
2022-03-17 14:43:19 +0800

    "First practice —— WordCount"

--- src/main/java/com/liu/practice/WordCountMapper.java
+++ src/main/java/com/liu/practice/WordCountMapper.java
@@ -1,4 +1,24 @@
 package com.liu.practice;
 
-public class WordCountMapper {
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+public class WordCountMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+
+    private final Text textWord = new Text();
+    private final IntWritable one = new IntWritable(1);
+
+    @Override
+    protected void map(LongWritable key, Text value, Mapper<LongWritable, Text, Text, IntWritable>.Context context) throws IOException, InterruptedException {
+        String line = value.toString();
+        String[] words = line.split(" ");
+        for (String word : words) {
+            textWord.set(word);
+            context.write(textWord, one);
+        }
+    }
 }
--- src/main/java/com/liu/practice/WordCountReducer.java
+++ src/main/java/com/liu/practice/WordCountReducer.java
@@ -1,4 +1,20 @@
 package com.liu.practice;
 
-public class WordCountReducer {
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+
+public class WordCountReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+    private final IntWritable cnt = new IntWritable(0);
+    @Override
+    protected void reduce(Text key, Iterable<IntWritable> values, Reducer<Text, IntWritable, Text, IntWritable>.Context context) throws IOException, InterruptedException {
+        int sum = 0;
+        for (IntWritable value : values) {
+            sum += value.get();
+        }
+        cnt.set(sum);
+        context.write(key, cnt);
+    }
 }
